ee5e6333f49270503e756ca5dfb8cf9901c31e0d
CeleritasCelery <t.macman@gmail.com>
2020-02-03 15:40:16 -0800

    Update stf packet alias to handle any log file

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -36,8 +36,5 @@ alias valid-json='python3.6.3 -m json.tool >/dev/null < '
 alias run_dft_ipgen='$DFT_REPO_ROOT/DFTNetworkGen/run_dft_ipgen'
 
 alias real-failures="echo 'BFM failure:'; ag --search-zip --depth 1 -G postsim.log -l '(?:jtagbfm|stf_bfm_agent)'; echo 'early termination:'; ag --search-zip --depth 1 -G postsim.log -l '(?:requiredtext not found:)'"
-alias stf-packets="ag --search-zip --nonumbers '(?:^ovm_error.+?stf_bfm_driver|^actual: |^expect: |^  mask: |^ovm_info.+?\[spf_itpp_parser_info\] \(\d+\)|^ovm_info.+?stf_bfm_driver.+?expected packet passed)' acerun.log.gz"
+alias stf-packets="ag --search-zip --nonumbers '(?:^ovm_error.+?stf_bfm_driver|^actual: |^expect: |^  mask: |^ovm_info.+?\[spf_itpp_parser_info\] \(\d+\)|^ovm_info.+?stf_bfm_driver.+?expected packet passed)'"
 alias spftmp='for i in *.espf; do $SPF_ROOT/bin/spf_perl_pp -stfSpecFile $STF_SPFSPEC -tapSpecFile $TAP_SPFSPEC -testSeqFile $i --itppFile $i.itpp --templateFile $DFT_GLOBAL_DIR/spf_sequences.1p0/tap/sub_system/TAP ; done'
-
-alias use-spacemacs='ln -nsfv /nfs/site/home/tjhinckl/custom/spacemacs-emacs ~/.emacs.d'
-alias use-custom='ln -nsfv /nfs/site/home/tjhinckl/custom/emacs.d ~/.emacs.d'
